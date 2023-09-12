1c149afa95e4b440c74b1d31e313c7b4a02157af
MagnetarPipa <GITHUB you@example.com>
2023-09-12 12:22:33 +0300

    Добавьте файлы проекта.

---
+++ Lab0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 3;
+            Math.Pow(x,y);
+            Console.WriteLine("Возведение в степень целого чилса: "+Math.Pow(x, y));
+            Console.ReadLine();
+        }
+    }
+}
