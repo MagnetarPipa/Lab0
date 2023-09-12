bb7e7db810afddcb7ce5add22f14480bee7242c1
MagnetarPipa <GITHUB you@example.com>
2023-09-12 15:52:53 +0300

    Added summary & random numbers

--- Lab0/Program.cs
+++ Lab0/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,24 @@ namespace Lab0
 {
     internal class Program
     {
+        /// <summary>
+        /// 18. Реализовать функцию возведения в куб целого числа
+        /// </summary>
+        /// <param name="x">Число которое возводим в степень</param>
+        /// <returns>возведение в куб целого числа</returns>
+        private static double Power3(int x)
+        {
+            return Math.Pow(x, 3);
+        }
+           
         static void Main(string[] args)
         {
-            int x = 5, y = 3;
-            Console.WriteLine("Возведение в степень целого чилса: "+Math.Pow(x, y));
+        
+            Random rnd = new Random();
+            int x = rnd.Next(-10,10);
+            Console.WriteLine($"Число в которое возводиться степерь:{x}; \nВозведение в степень целого чилса: { Power3(x)}");
             Console.ReadLine();
+
         }
     }
 }
